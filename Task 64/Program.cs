fa9a44e41484631262954660bb36c9179d93ec9f
Sergey Savchenko <cwsilentic@gmail.com>
2023-01-15 15:42:03 +0300

    Homework

---
+++ Task 64/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 64: Написать рекурсивную функцию перевода из десятичной системы исчисления в двоичную.
+
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+
+string BinarySystem (int num)
+{
+    if (num != 0)
+    {
+        return BinarySystem(num/2) + num % 2;
+    }
+    else return "";
+}
+
+System.Console.Write("Введите число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+string answer = BinarySystem(m);
+Console.Write($"Результирующее двоичное число: {answer}");
---
+++ Task 66/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int CountNumbers(int x, int y)
+{
+    if(Math.Max(x,y) != Math.Min(x,y))
+        return CountNumbers(Math.Max(x,y),Math.Min(x,y)+1) + Math.Min(x,y);
+    else return Math.Max(x,y);
+}
+
+System.Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int numb = CountNumbers(n,m);
+Console.WriteLine($"Сумма чисел между M и N: {numb}");
